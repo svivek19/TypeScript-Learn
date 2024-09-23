84a79ef1e31a81425f69285012872ed95feaa40c
svivek19 <svivek030503@gmail.com>
2024-09-23 14:27:41 +0530

    Array

---
+++ dest/Arrays.js
@@ -0,0 +1,16 @@
+"use strict";
+// Arrays
+// Implicit
+// let arr = [1, 2, 3];
+// let str = ["a", "b", "c"];
+// let bool = [true, false];
+// Explicit
+let arr = [1, 2, 3];
+let str = ["a", "b", "c"];
+let bool = [true, false];
+//Declare and assign later
+let arr2 = [];
+arr2.push(1);
+//Readonly
+let arr3 = [1, 2, 3];
+// arr3.push(1);
---
+++ src/Arrays.ts
@@ -0,0 +1,22 @@
+// Arrays
+
+// Implicit
+
+// let arr = [1, 2, 3];
+// let str = ["a", "b", "c"];
+// let bool = [true, false];
+
+// Explicit
+
+let arr: number[] = [1, 2, 3];
+let str: string[] = ["a", "b", "c"];
+let bool: boolean[] = [true, false];
+
+//Declare and assign later
+let arr2: number[] = [];
+
+arr2.push(1);
+
+//Readonly
+let arr3: readonly number[] = [1, 2, 3];
+// arr3.push(1);
