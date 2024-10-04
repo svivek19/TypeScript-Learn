bb5596813c06ebfccd2a49171a021db95b5103bf
svivek19 <svivek030503@gmail.com>
2024-10-04 16:49:45 +0530

    tuples

---
+++ src/Tuples.ts
@@ -0,0 +1,28 @@
+// Tuples
+
+// let arr5 = ["a", 40, true];
+
+// arr5[0] = "b";
+
+// console.log(arr5);
+
+// Tuples is typed array
+// let arr5: readonly [string, number, boolean] = ["a", 40, true];
+
+// // arr5[0] = 50;
+// arr5.push(50);
+
+// console.log(arr5);
+
+//Named
+
+// let data: readonly [name: string, age: number, isEng: boolean] = [
+//   "joe",
+//   20,
+//   true,
+// ];
+
+// Destructuring
+// let [userName, age, isEng] = data;
+
+// console.log(userName, age, isEng);
